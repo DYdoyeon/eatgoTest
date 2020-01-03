b9ebb59035fc6a120f2094ca1419e865ff5d3f2e
DoyeonKim <doyeon.kim0208@gmail.com>
2020-01-03 21:27:01 +0900

    hello world

---
+++ src/main/java/kr/co/fastcampus/eatgo/interfaces/WelcomeController.java
@@ -0,0 +1,14 @@
+package kr.co.fastcampus.eatgo.interfaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	@GetMapping("/")
+	public String helloWorld(){
+		return "Hello World";
+	}
+}
