cd61d108435e97b155b5363512ef7e78244b4be7
DoyeonKim <doyeon.kim0208@gmail.com>
2020-01-16 23:39:52 +0900

    separate layer through service

--- src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
+++ src/main/java/kr/co/fastcampus/eatgo/domain/Restaurant.java
@@ -1,17 +1,24 @@
 package kr.co.fastcampus.eatgo.domain;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Restaurant {
     private final String name;
     private final String address;
+    private final Long id;
+    private List<MenuItem> menuItems=new ArrayList<MenuItem>();
 
-    public Restaurant(String name, String address) {
-    this.name = name;
-    this.address = address;
+    public Restaurant(Long id, String name, String address) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
     
     }
 
     public String getName() {
-        return "Bob zip";
+        return name;
     }
 
     public String getInformation() {
@@ -20,5 +27,21 @@ public class Restaurant {
 
     public String getAddress() {
         return address;
+    }
+    public Long getId(){
+        return id;
+    }
+
+    public void addMenuItem(MenuItem menuItem) {
+        menuItems.add(menuItem);
+    }
+    public List<MenuItem> getMenuItems(){
+        return menuItems;
+    }
+
+    public void setMenuItems(List<MenuItem> menuItems) {
+        for(MenuItem menuItem : menuItems){
+            addMenuItem(menuItem);
+        }
     }
 }
