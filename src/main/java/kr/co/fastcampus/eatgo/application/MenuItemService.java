3bc71540f9aa37e1a241cc19c21884f3530f4883
DYdoyeon <doyeon.kim0208@gmail.com>
2020-02-13 18:26:06 +0900

    20200213 - add menuItem

---
+++ src/main/java/kr/co/fastcampus/eatgo/application/MenuItemService.java
@@ -0,0 +1,17 @@
+package kr.co.fastcampus.eatgo.application;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.fastcampus.eatgo.domain.MenuItem;
+
+@Service
+public class MenuItemService{
+
+	public void bulkUpdate(Long restaurantId,List<MenuItem> menuItems)  {
+	
+	}
+
+
+}
